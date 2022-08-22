bb38ed7487635249f57b3984053ea5029eb7972c
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-08-22 16:58:07 -0400

    save

---
+++ unit1APCSA.java
@@ -0,0 +1,13 @@
+public class unit1APCSA {
+
+    public static void main(String[] args){
+
+        System.out.println("Testing");
+        System.out.println("");
+        System.out.print("Same");
+        System.out.print("Line");
+
+
+    }
+    
+}
