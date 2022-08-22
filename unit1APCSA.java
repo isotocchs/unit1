0101d8f8e37aad4ef2b856a4f5f26d3c2573659f
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-08-22 17:04:28 -0400

    save

--- unit1APCSA.java
+++ unit1APCSA.java
@@ -1,4 +1,10 @@
-public class unit1APCSA {
+
+
+public class unit1APCSA {  //class name must match file name
+
+    // this is a comment
+    // to comment something use (comand + /)
+    // make sure to comment your code often
 
     public static void main(String[] args){
 
@@ -6,6 +12,20 @@ public class unit1APCSA {
         System.out.println("");
         System.out.print("Same");
         System.out.print("Line");
+
+        // everything within the " " is what is printed. There is no spellcheck
+
+        System.out.println("Thisiis is a test");
+
+        //even spaces, be careful with spaces
+
+        System.out.print("This");
+        System.out.print("needs");
+        System.out.print("spaces");
+
+
+
+        
 
 
     }
