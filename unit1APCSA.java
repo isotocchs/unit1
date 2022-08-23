f4ad2a5a83e09407687863cbb48068e1376a11e8
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-08-22 20:45:26 -0400

    save

--- unit1APCSA.java
+++ unit1APCSA.java
@@ -24,6 +24,40 @@ public class unit1APCSA {  //class name must match file name
         System.out.print("spaces");
 
 
+        //variables and data types
+        System.out.println("");
+        System.out.println("-----------");
+        System.out.println("-----------");
+
+        // you create them in Java using the datatype and then variable name
+
+        // intigers, whole numbers - int
+        int thisIsAVariable = 10;
+        System.out.println(thisIsAVariable);
+
+        //double used to decimals
+        double thisIsADecimal = 13.6;
+        System.out.println(thisIsADecimal);
+
+        //boolean used for true or false
+        boolean trueOrFalse = true;
+        System.out.println(trueOrFalse);
+
+        // if we want to make a constant then we use the keyword final
+        final double pi = 3.14;
+        System.out.println(pi);
+
+        //you can also initialize a variable placeholder and use it later.
+        double someDecimal;
+        someDecimal = 45.89;
+        System.out.println(someDecimal);
+
+
+
+
+
+
+
 
         
 
