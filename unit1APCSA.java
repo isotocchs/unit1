47e6331bfc487306f2144fd67c77caa2abb2ce2d
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-08-24 12:09:17 -0400

    save

--- unit1APCSA.java
+++ unit1APCSA.java
@@ -8,6 +8,7 @@ public class unit1APCSA {  //class name must match file name
 
     public static void main(String[] args){
 
+
         System.out.println("Testing");
         System.out.println("");
         System.out.print("Same");
@@ -15,16 +16,16 @@ public class unit1APCSA {  //class name must match file name
 
         // everything within the " " is what is printed. There is no spellcheck
 
-        System.out.println("Thisiis is a test");
+        System.out.println("Thisiis is a test ksrhfgkjw");
 
         //even spaces, be careful with spaces
 
-        System.out.print("This");
-        System.out.print("needs");
-        System.out.print("spaces");
+        System.out.print("This ");
+        System.out.print("needs ");
+        System.out.print("spaces.");
 
 
-        //variables and data types
+        // //variables and data types
         System.out.println("");
         System.out.println("-----------");
         System.out.println("-----------");
@@ -32,25 +33,57 @@ public class unit1APCSA {  //class name must match file name
         // you create them in Java using the datatype and then variable name
 
         // intigers, whole numbers - int
-        int thisIsAVariable = 10;
-        System.out.println(thisIsAVariable);
+        int something = 10;
+        System.out.println(something);
+        System.out.println("-----------");
 
-        //double used to decimals
-        double thisIsADecimal = 13.6;
+        // //double used to decimals
+        double thisIsADecimal = 13;
         System.out.println(thisIsADecimal);
+        System.out.println("-----------");
 
-        //boolean used for true or false
+        thisIsADecimal = something;
+        System.out.println(thisIsADecimal);
+        System.out.println("-----------");
+
+
+        // //boolean used for true or false
         boolean trueOrFalse = true;
         System.out.println(trueOrFalse);
+        System.out.println("-----------");
 
         // if we want to make a constant then we use the keyword final
         final double pi = 3.14;
         System.out.println(pi);
+        System.out.println("-----------");
+
+        // Strings are words
+        String someWords = "This is a sentence. @";
+        System.out.println(someWords);
+        System.out.println("-----------");
+
 
         //you can also initialize a variable placeholder and use it later.
         double someDecimal;
         someDecimal = 45.89;
         System.out.println(someDecimal);
+        System.out.println("-----------");
+
+
+        int a = 45;
+        double b = 5;
+        double c = b/a;
+        System.out.println(c);
+        System.out.println("-----------");
+
+
+        thisIsADecimal = 13.0/55;
+        System.out.println(thisIsADecimal);
+        System.out.println("-----------");
+
+        thisIsADecimal = 5+(3*4);
+        System.out.println(thisIsADecimal);
+        System.out.println("-----------");
 
 
 
