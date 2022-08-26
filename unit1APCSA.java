85e65e9d71ae1a02692c1ffd656583fb67ba1a7c
isotocchs <89474695+isotocchs@users.noreply.github.com>
2022-08-26 11:09:37 -0400

    save

--- unit1APCSA.java
+++ unit1APCSA.java
@@ -8,21 +8,23 @@ public class unit1APCSA {  //class name must match file name
 
     public static void main(String[] args){
 
+        int numbers;
 
-        System.out.println("Testing");
-        System.out.println();
-        System.out.println("Same thing in the class");
-        System.out.println(" Line");
 
-        // everything within the " " is what is printed. There is no spellcheck
+        // System.out.println("Testing");
+        // System.out.println();
+        // System.out.println("Same thing in the class");
+        // System.out.println(" Line");
 
-        System.out.println("Thisiis is a test ksrhfgkjw");
+        // // everything within the " " is what is printed. There is no spellcheck
 
-        //even spaces, be careful with spaces
+        // System.out.println("Thisiis is a test ksrhfgkjw");
 
-        System.out.print("This ");
-        System.out.print("needs ");
-        System.out.print("spaces.");
+        // //even spaces, be careful with spaces
+
+        // System.out.print("This ");
+        // System.out.print("needs ");
+        // System.out.print("spaces.");
 
 
         // //variables and data types
@@ -33,36 +35,50 @@ public class unit1APCSA {  //class name must match file name
         // you create them in Java using the datatype and then variable name
 
         // intigers, whole numbers - int
-        int numbers;
-        numbers=10;
+        
+        numbers = 10;
+        
+        System.out.println(numbers);
+
+        numbers = 56;
         System.out.println(numbers);
         System.out.println("-----------");
 
         // //double used to decimals
-        double thisIsADecimal = 13.0000010000;
+        double thisIsADecimal = 13.00000001;
         System.out.println(thisIsADecimal);
         System.out.println("-----------");
 
-        thisIsADecimal = numbers;
-        System.out.println(thisIsADecimal);
-        System.out.println("-----------");
+        // thisIsADecimal = numbers;
+        // System.out.println(thisIsADecimal);
+        // System.out.println("-----------");
 
 
-        // // //boolean used for true or false
-        boolean trueOrFalse = false;
+        // // // //boolean used for true or false
+        int x = 34;
+        double y = 67.9;
+        boolean trueOrFalse = x<y;
         System.out.println(trueOrFalse);
         System.out.println("-----------");
 
         // if we want to make a constant then we use the keyword final
-        // final double pi = 3.14;      
-        // System.out.println(pi);
-        // System.out.println("-----------");
+        final double pi = 3.14;      
+        System.out.println(pi);
+        System.out.println("-----------");
 
         // // // Strings are words
-        // String someWords = "Tomorrow is Saturday ";
-        // char somethingElse = 'r';
-        // System.out.println(someWords);
-        // System.out.println("-----------");
+        String user = "Bob";
+        int age = 57;
+        double gpa = 4.5;
+        System.out.println(user+" is "+age+" and his GPA is "+gpa);
+        System.out.println("-----------");
+
+        String admin = "Mr Soto";
+        char somethingElse = 'r';
+        String something = "Fun";
+        String admin2 = admin+" "+somethingElse+" "+something;
+        System.out.println(admin2);
+        System.out.println("-----------");
 
 
         // //you can also initialize a variable placeholder and use it later.
@@ -72,36 +88,39 @@ public class unit1APCSA {  //class name must match file name
         // System.out.println("-----------");
 
 
-        // int a = 8;
-        // double b = 3;
-        // double c = a/b;
-        // System.out.println(c);
-        // System.out.println("-----------");
+        int a = 8;
+        double b = -4;
+        double c = a/b;
+        System.out.println(c);
+        System.out.println("-----------");
+        
 
-
-        // thisIsADecimal = 13/55;
+        // thisIsADecimal = 13.0000/55.0;
         // System.out.println(thisIsADecimal);
         // System.out.println("-----------");
 
-        // thisIsADecimal = 5+(3*4)%6;
+        // thisIsADecimal = 5+(3*4)/6;
         // System.out.println(thisIsADecimal);
         // System.out.println("-----------");
 
-        // The difference between numbers and strings
+        //The difference between numbers and strings
         System.out.println(6);
-        System.out.println("6");
+        System.out.println("Tom is "+(6+6));
         System.out.println(6+6);
-        System.out.println("T\"\""+"o"+"m"+"a"+"s");
+        System.out.println("6"+"6");
+
+
+        //System.out.println("T\"\""+"o"+"m"+"a"+"s");
         //System.out.println("-----------");
 
-        // assignement operator (=) go left to right
-        int apple = 4;
-        int x = apple = 10;
-        int y = x = apple=14;
-        System.out.println(x);
-        System.out.println(apple);
-        //System.out.println(y);
-        System.out.println("-----------");
+        // // assignement operator (=) go left to right
+        // int apple = 4;
+        // int x = apple = 10;
+        // int y = x = apple=14;
+        // System.out.println(x);
+        // System.out.println(apple);
+        // //System.out.println(y);
+        // System.out.println("-----------");
 
 
 
